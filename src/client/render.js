9c13620812cc142c4cc5132e4d8fdf9a51ac7f05
CarrotzRule123 <48111396+CarrotzRule123@users.noreply.github.com>
2020-04-06 16:19:39 +0800

    Update render.js

--- src/client/render.js
+++ src/client/render.js
@@ -3,10 +3,14 @@
 import { debounce } from 'throttle-debounce';
 import { getAsset } from './assets';
 import { getCurrentState } from './state';
+const AnimalConstants = require('../shared/animal-constants');
+
+var p = document.getElementById('p');
+var count = document.getElementById('count');
 
 const Constants = require('../shared/constants');
 
-const { PLAYER_RADIUS, PLAYER_MAX_HP, BULLET_RADIUS, MAP_SIZE } = Constants;
+const { PLAYER_RADIUS, BULLET_RADIUS, MAP_WIDTH, MAP_HEIGHT } = Constants;
 
 // Get the canvas graphics context
 const canvas = document.getElementById('game-canvas');
@@ -24,18 +28,23 @@ function setCanvasDimensions() {
 window.addEventListener('resize', debounce(40, setCanvasDimensions));
 
 function render() {
-  const { me, others, bullets } = getCurrentState();
+  const { me, others, bullets, xp, info} = getCurrentState();
+  me.stats = AnimalConstants[me.tier-1][me.animal]
+  others.forEach(o => {o.stats = AnimalConstants[o.tier-1][o.animal]});
   if (!me) {
     return;
   }
 
   // Draw background
   renderBackground(me.x, me.y);
+  xp.forEach(index => renderXp(me,index))
+  renderXpBar(me)
+  renderText(me)
 
   // Draw boundaries
   context.strokeStyle = 'black';
   context.lineWidth = 1;
-  context.strokeRect(canvas.width / 2 - me.x, canvas.height / 2 - me.y, MAP_SIZE, MAP_SIZE);
+  context.strokeRect(canvas.width / 2 - me.x, canvas.height / 2 - me.y, MAP_WIDTH, MAP_HEIGHT);
 
   // Draw all bullets
   bullets.forEach(renderBullet.bind(null, me));
@@ -45,35 +54,36 @@ function render() {
   others.forEach(renderPlayer.bind(null, me));
 }
 
+function renderText(me){
+  p.innerHTML = "You are a "+me.stats.name
+  const score = Math.round(me.score)
+  const stats = me.stats.upgrade 
+  count.innerHTML = score +' out of '+ stats +' xp('+ (stats-score) + 'xp remaining to upgrade)'
+}
+
 function renderBackground(x, y) {
-  const backgroundX = MAP_SIZE / 2 - x + canvas.width / 2;
-  const backgroundY = MAP_SIZE / 2 - y + canvas.height / 2;
-  const backgroundGradient = context.createRadialGradient(
-    backgroundX,
-    backgroundY,
-    MAP_SIZE / 10,
-    backgroundX,
-    backgroundY,
-    MAP_SIZE / 2,
-  );
-  backgroundGradient.addColorStop(0, 'black');
-  backgroundGradient.addColorStop(1, 'gray');
-  context.fillStyle = backgroundGradient;
+  context.fillStyle = "white";
   context.fillRect(0, 0, canvas.width, canvas.height);
+  const mapX = 0 - x + canvas.width / 2;
+  const mapY = 0 - y + canvas.height / 2;
+  context.drawImage(
+    getAsset('map.png'),
+    mapX, mapY,
+  );
+  context.restore();
 }
 
 // Renders a ship at the given coordinates
 function renderPlayer(me, player) {
-  const { x, y, direction } = player;
+  const { x, y, direction} = player;
   const canvasX = canvas.width / 2 + x - me.x;
   const canvasY = canvas.height / 2 + y - me.y;
 
-  // Draw ship
   context.save();
   context.translate(canvasX, canvasY);
   context.rotate(direction);
   context.drawImage(
-    getAsset('ship.svg'),
+    getAsset(player.stats.img),
     -PLAYER_RADIUS,
     -PLAYER_RADIUS,
     PLAYER_RADIUS * 2,
@@ -91,10 +101,21 @@ function renderPlayer(me, player) {
   );
   context.fillStyle = 'red';
   context.fillRect(
-    canvasX - PLAYER_RADIUS + PLAYER_RADIUS * 2 * player.hp / PLAYER_MAX_HP,
+    canvasX - PLAYER_RADIUS + PLAYER_RADIUS * 2 * player.hp / player.stats.hp,
     canvasY + PLAYER_RADIUS + 8,
-    PLAYER_RADIUS * 2 * (1 - player.hp / PLAYER_MAX_HP),
+    PLAYER_RADIUS * 2 * (1 - player.hp / player.stats.hp),
     2,
+  );
+}
+
+function renderXpBar(me) {
+  context.fillStyle = 'grey';
+  context.fillRect(
+    100, canvas.height-75, canvas.width-200, 20
+  );
+  context.fillStyle = 'yellow';
+  context.fillRect(
+    100, canvas.height-75, (canvas.width-200)* (me.score / me.stats.upgrade), 20
   );
 }
 
@@ -109,10 +130,20 @@ function renderBullet(me, bullet) {
   );
 }
 
+function renderXp(me,xp) {
+  const {x, y} = xp;
+  const xpX = x - me.x + canvas.width / 2;
+  const xpY = y - me.y + canvas.height / 2;
+  context.fillStyle = "lime";
+  context.beginPath()
+  context.arc(xpX, xpY, 10, 0, 2 * Math.PI);
+  context.fill()
+}
+
 function renderMainMenu() {
   const t = Date.now() / 7500;
-  const x = MAP_SIZE / 2 + 800 * Math.cos(t);
-  const y = MAP_SIZE / 2 + 800 * Math.sin(t);
+  const x = MAP_WIDTH / 2 + 800 * Math.cos(t);
+  const y = MAP_HEIGHT / 2 + 800 * Math.sin(t);
   renderBackground(x, y);
 }
 
