95d907e87dbb49408816ebecec99abe6243a559c
CarrotzRule123 <48111396+CarrotzRule123@users.noreply.github.com>
2020-04-06 16:25:30 +0800

    Create animal-constants.js

---
+++ src/shared/animal-constants.js
@@ -0,0 +1,29 @@
+module.exports = Object.freeze([
+  [
+    {
+      name: "Sniffler",
+      img: "sniffler.png",
+      hp: 150,
+      dmg: 10,
+      upgrade:50,
+    }
+  ],
+  [
+    {
+      name: "Coral Pincher",
+      img: "pincher.png",
+      hp: 150,
+      dmg: 40,
+      upgrade:100,
+    }
+  ],
+  [
+    {
+      name: "Common Drifter",
+      img: "drifter.png",
+      hp: 200,
+      dmg: 40,
+      upgrade:200,
+    }
+  ]
+]);
